50a50abca957796f23ec5eb2543d6edbd9583d8a
yopaseopor <yopaseopor@gmail.com>
2020-08-20 12:10:27 +0200

    Esri

--- src/config.js
+++ src/config.js
@@ -114,6 +114,15 @@ var config = {
 			visible: false
 		}),
 		new ol.layer.Tile({
+			title: 'Esri Sat',
+			iconSrc: imgSrc + 'esri_logo_layer.png',
+			source: new ol.source.XYZ({
+				attributions: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+				url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+			}),
+			visible: false
+		}),
+		new ol.layer.Tile({
 			title: 'Google Maps',
 			iconSrc: imgSrc + 'gmaps_logo_layer.png',
 			source: new ol.source.XYZ({
