7e6c4d6377b65da1f6dbd873c246f9578a46120d
yopaseopor <yopaseopor@gmail.com>
2022-07-24 18:03:39 +0200

    update code info

--- src/config.js
+++ src/config.js
@@ -2025,7 +2025,7 @@ var config = {
 					}),
 							text: new ol.style.Text({
 								text: name,
-								font: 'small-caps bold 18px/1 sans-serif',
+								font: 'small-caps bold 10px/1 sans-serif',
 								offsetX : 0,
 								offsetY : 0,
 								fill: new ol.style.Fill({
